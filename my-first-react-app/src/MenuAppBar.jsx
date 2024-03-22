8698ae410db56028a7296fd7588f58eb49086be3
maxhillebr <mymail123@mail.de>
2024-03-22 09:57:05 +0100

    added the drawer onClick for the main menu; works

--- my-first-react-app/src/MenuAppBar.jsx
+++ my-first-react-app/src/MenuAppBar.jsx
@@ -4,13 +4,13 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import Switch from "@mui/material/Switch";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import TemporaryDrawer from "./TemporaryDrawer.jsx";
 
 export default function MenuAppBar() {
   const [auth, setAuth] = React.useState(true);
@@ -44,15 +44,7 @@ export default function MenuAppBar() {
       </FormGroup>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <TemporaryDrawer />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Photos
           </Typography>
