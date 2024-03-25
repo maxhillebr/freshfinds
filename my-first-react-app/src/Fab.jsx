d3f61a9424cbee90261b1fa9eb8fa279bd0b7998
Max Hillebrand <max.hillebrand@yahoo.de>
2024-03-25 11:08:03 +0100

    fixed routing installation

--- my-first-react-app/src/Fab.jsx
+++ my-first-react-app/src/Fab.jsx
@@ -1,12 +1,23 @@
 import * as React from "react";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
+// import { useState } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import NewList from "./NewList.jsx";
 
 export default function FloatingActionButtons() {
   return (
     <Fab variant="extended" color="primary" aria-label="add">
-      NEW
-      <AddIcon />
+      <Router>
+        <Link to="/new">New</Link>
+        <AddIcon />
+
+        <Switch>
+          <Route path="/new">
+            <NewList />
+          </Route>
+        </Switch>
+      </Router>
     </Fab>
   );
 }
---
+++ my-first-react-app/src/NewList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function NewList() {
+  return (
+    <div>
+      <h2>Add New Item</h2>
+      {/* Your form for adding new items goes here */}
+      {/* For example:*/}
+      <form>
+        <label>Title:</label>
+        <input type="text" name="title" />
+        <label>Description:</label>
+        <textarea name="description"></textarea>
+        <button type="submit">Add Item</button>
+      </form>
+    </div>
+  );
+}
