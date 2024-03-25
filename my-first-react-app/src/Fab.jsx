4a22e21f5825c2ec0a0d3a9c5481f8dc1fa88bff
Max Hillebrand <max.hillebrand@yahoo.de>
2024-03-25 11:29:13 +0100

    small changes

--- my-first-react-app/src/Fab.jsx
+++ my-first-react-app/src/Fab.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 
