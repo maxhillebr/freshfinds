7183897cc3b34b8bb959e290e4db51cfea67d42c
Max Hillebrand <max.hillebrand@yahoo.de>
2024-03-25 12:43:21 +0100

    added first table layout from mui

--- my-first-react-app/src/NewList.jsx
+++ my-first-react-app/src/NewList.jsx
@@ -1,16 +1,46 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import GroceryTable from "./GroceryTable";
+
 export default function NewList() {
   return (
     <div>
-      <h2>Add New Item</h2>
-      {/* Your form for adding new items goes here */}
-      {/* For example:*/}
-      <form>
-        <label>Title:</label>
-        <input type="text" name="title" />
-        <label>Description:</label>
-        <textarea name="description"></textarea>
-        <button type="submit">Add Item</button>
-      </form>
+      <h2>Create New Grocery List</h2>
+      <div className="form-box">
+        <TextField
+          required
+          id="grocery-list-title"
+          label="Title"
+          defaultValue=""
+        />
+        <TextField
+          required
+          id="grocery-list-description"
+          label="Desciption"
+          defaultValue=""
+        />
+      </div>
+      <h2>Add Products</h2>
+      <div className="form-box">
+        <TextField
+          required
+          id="grocery-list-product"
+          label="Product"
+          defaultValue=""
+        />
+        <TextField
+          required
+          id="grocery-list-amount"
+          label="Amount"
+          defaultValue=""
+        />
+      </div>
+      <Button variant="contained">Add</Button>
+
+      <div>
+        <GroceryTable />
+      </div>
     </div>
   );
 }
