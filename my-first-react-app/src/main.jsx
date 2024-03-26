69ae81df910cca7f799e606465c513050924dd22
Max Hillebrand <max.hillebrand@yahoo.de>
2024-03-26 11:41:32 +0100

    housekeeping

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -5,20 +5,20 @@ import Routing from "./Routing.jsx";
 
 import "./index.css";
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB5NbqVvvMRbelJnyh95k13NzCazeUUQ6g",
-//   authDomain: "freshfinds-3456a.firebaseapp.com",
-//   projectId: "freshfinds-3456a",
-//   storageBucket: "freshfinds-3456a.appspot.com",
-//   messagingSenderId: "1016837431826",
-//   appId: "1:1016837431826:web:3d73c232b3e47885e5eecb",
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5NbqVvvMRbelJnyh95k13NzCazeUUQ6g",
+  authDomain: "freshfinds-3456a.firebaseapp.com",
+  projectId: "freshfinds-3456a",
+  storageBucket: "freshfinds-3456a.appspot.com",
+  messagingSenderId: "1016837431826",
+  appId: "1:1016837431826:web:3d73c232b3e47885e5eecb",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
