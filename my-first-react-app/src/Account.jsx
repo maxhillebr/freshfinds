ebedfc2a128673bf570b4b8e42bb402da89dac69
Max Hillebrand <max.hillebrand@yahoo.de>
2024-03-28 10:38:54 +0100

    update to account, displays name/mail via getAuth

--- my-first-react-app/src/Account.jsx
+++ my-first-react-app/src/Account.jsx
@@ -1,8 +1,17 @@
-export default function Account() {
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
+
+const Account = () => {
+  const user = auth.currentUser;
+
   return (
     <div className="main-content">
       <h2>Your Account</h2>
-      Name Mail
+      <p>Username: {user.displayName}</p>
+      <p>Mail: {user.email}</p>
     </div>
   );
-}
+};
+
+export default Account;
