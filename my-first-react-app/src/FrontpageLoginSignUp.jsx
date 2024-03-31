b437d1de1fb4425ea14720a3e8f853d12ee79a9c
maxhillebr <mymail123@mail.de>
2024-03-31 08:43:16 +0200

    added new logo and container for first page

--- my-first-react-app/src/FrontpageLoginSignUp.jsx
+++ my-first-react-app/src/FrontpageLoginSignUp.jsx
@@ -1,15 +1,25 @@
 import Button from "@mui/material/Button";
+import { spacing } from "@mui/system";
 
 const FrontpageLoginSignUp = () => {
   return (
     <>
-      <p>Hello to freshfinds!</p>
-      <Button href="/signup" variant="contained">
-        Sign up
-      </Button>
-      <Button href="/login" variant="contained">
-        Login
-      </Button>
+      <div className="first-pages-flex-container">
+        <div>
+          <img
+            src="\src\assets\freshfinds-logo.png"
+            alt="freshfinds Logo"
+            width={"200px"}
+          />
+        </div>
+        <p>Hello to freshfinds!</p>
+        <Button href="/signup" variant="contained" sx={{ m: 1 }}>
+          Sign up
+        </Button>
+        <Button href="/login" variant="outlined" sx={{ m: 1 }}>
+          Login
+        </Button>
+      </div>
     </>
   );
 };
