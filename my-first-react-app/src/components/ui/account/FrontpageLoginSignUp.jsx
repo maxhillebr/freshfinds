2613d79df075cb04afcb947547a976a0bb7bcfee
Max Hillebrand <max.hillebrand@yahoo.de>
2024-04-19 07:58:52 +0200

    new theme for buttons, changed layout of the start page

--- my-first-react-app/src/components/ui/account/FrontpageLoginSignUp.jsx
+++ my-first-react-app/src/components/ui/account/FrontpageLoginSignUp.jsx
@@ -1,23 +1,44 @@
 import Button from "@mui/material/Button";
+import "/src/css/main.css";
+import "/src/css/login-signup.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const FrontpageLoginSignUp = () => {
   return (
     <>
-      <div className="first-pages-flex-container">
-        <div>
-          <img
-            src="/freshfinds-logo.png"
-            alt="freshfinds Logo"
-            style={{ width: "200px" }}
-          />
+      <div className="content">
+        <div className="content-main">
+          <div className="logo-container">
+            <img src="/freshfinds-logo.png" alt="freshfinds logo" />
+          </div>
+          <div className="entry-text">
+            <p>The grocery list & recipe app made to share.</p>
+          </div>
+          <div className="button-container">
+            <Button href="/signup" variant="contained">
+              Create a profile
+            </Button>
+            <p className="button-container__text">or</p>
+
+            <a href="/login">Login to your account.</a>
+          </div>
+          <div className="credentials-container">
+            <div>
+              <GitHubIcon />
+            </div>
+            <div>
+              <LinkedInIcon />
+            </div>
+          </div>
+          <div className="bottom-illustration">
+            <img
+              src="/illustrations/undraw_eating_together_re_ux62.svg"
+              alt="cooking together"
+              width={"200px"}
+            />
+          </div>
         </div>
-        <p>Hello to freshfinds!</p>
-        <Button fullWidth href="/signup" variant="contained" sx={{ m: 1 }}>
-          Sign up
-        </Button>
-        <Button fullWidth href="/login" variant="outlined" sx={{ m: 1 }}>
-          Login
-        </Button>
       </div>
     </>
   );
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -3,9 +3,32 @@ import ReactDOM from "react-dom/client";
 import Routing from "../src/Routing.jsx";
 import "./index.css";
 
+import { createTheme, ThemeProvider } from "@mui/material";
+
+// Define a custom theme with your main color
+const freshfindsTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#5556F1", // Your main color
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          padding: "16px 38px", // Customize padding
+          borderRadius: "24px", // Customize border radius
+        },
+      },
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   // Removed strictmode because of react-beautiful-dnd
   // <React.StrictMode>
-  <Routing />
+  <ThemeProvider theme={freshfindsTheme}>
+    <Routing />
+  </ThemeProvider>
   // </React.StrictMode>
 );
