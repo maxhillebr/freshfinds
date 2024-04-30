410d7679e6d5e77ca6c86dc23064af7ee7efecf4
Max Hillebrand <max.hillebrand@yahoo.de>
2024-04-30 09:15:16 +0200

    fixed bottom nav css and added icon to indicate clickable element

--- my-first-react-app/src/components/ui/nav/NavBottom.jsx
+++ my-first-react-app/src/components/ui/nav/NavBottom.jsx
@@ -1,29 +1,72 @@
-import "/src/css/navbottom.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import HomeIcon from "@mui/icons-material/Home";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore"; // Import an icon that indicates a dropdown
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import "/src/css/navbottom.css";
 
 const NavBottom = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <nav className="bottom-nav">
       <ul>
         <li>
-          <a href="/home">
+          <Link to="/home">
             <HomeIcon />
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#">
-            <AddCircleOutlineIcon
-              fontSize="large"
-              style={{ color: "#1976D2" }}
+          <IconButton
+            aria-controls="create-new-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+            style={{ color: "#1976D2", position: "relative" }}
+          >
+            <AddCircleOutlineIcon fontSize="large" />
+            <ExpandMoreIcon
+              style={{
+                position: "absolute",
+                top: "6px",
+                right: "-4px",
+                fontSize: "medium",
+              }}
             />
-          </a>
+          </IconButton>
+          <Menu
+            id="create-new-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose} component={Link} to="/newmealplan">
+              Mealplan
+            </MenuItem>
+            <MenuItem onClick={handleClose} component={Link} to="/newlist">
+              Grocery List
+            </MenuItem>
+            <MenuItem onClick={handleClose} component={Link} to="/newrecipe">
+              Recipe
+            </MenuItem>
+          </Menu>
         </li>
         <li>
-          <a href="/account">
+          <Link to="/account">
             <SettingsIcon />
-          </a>
+          </Link>
         </li>
       </ul>
     </nav>
