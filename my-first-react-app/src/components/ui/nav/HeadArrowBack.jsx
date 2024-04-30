16b6d0939308218aa139b68325f344fe21028a07
Max Hillebrand <max.hillebrand@yahoo.de>
2024-04-30 09:01:59 +0200

    fixed back button; arrow icon now links to actual last page

--- my-first-react-app/src/components/ui/nav/HeadArrowBack.jsx
+++ my-first-react-app/src/components/ui/nav/HeadArrowBack.jsx
@@ -1,12 +1,21 @@
-import "/src/css/headarrowback.css";
+import { useNavigate } from "react-router-dom";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import "/src/css/headarrowback.css";
 
 const HeadArrowBack = () => {
+  const navigate = useNavigate(); // Access the navigate function
+
+  const goBack = () => {
+    navigate(-1); // Navigate back to the previous page
+  };
+
   return (
-    <div className="back-home-container">
-      <a href="/home">
-        <ArrowBackIosIcon />
-      </a>
+    <div
+      className="back-home-container"
+      onClick={goBack}
+      style={{ cursor: "pointer" }}
+    >
+      <ArrowBackIosIcon />
     </div>
   );
 };
